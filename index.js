ab5d02440bee3031c4bfc6866e135266e399d876
unknown <mdadib300@gmail.com>
2025-07-26 10:22:17 +0600

    First version server completed

---
+++ index.js
@@ -0,0 +1,274 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// -------------------- MongoDB --------------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@xpointcluster.j3qynmm.mongodb.net/?retryWrites=true&w=majority&appName=XpointCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    ///////////////////////////////// COLLECTIONS
+    const productsCollection = client.db("xpointDB").collection("productsCollection");
+    const cartCollection = client.db("xpointDB").collection("cartCollection");
+    const usersCollection = client.db("xpointDB").collection("usersCollection");
+    const ordersCollection = client.db("xpointDB").collection("ordersCollection");
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+      res.send({ token });
+    })
+
+    // Adding users to DB when they register
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // middleswares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    // admin check api
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    // View all users in server (ADMIN ONLY)
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const cursor = usersCollection.find();
+      const allUsers = await cursor.toArray();
+      res.send(allUsers);
+    })
+
+    // Delete user from dashboard (ADMIN ONLY)
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Make a user admin (DEVELOPER & ADMIN ONLY)
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // Displaying the products in the UI 
+    app.get('/products', async (req, res) => {
+      const cursor = productsCollection.find();
+      const allProducts = await cursor.toArray();
+      res.send(allProducts);
+    })
+
+    // adding products in the products server (ADMIN ONLY)
+    app.post('/products', verifyToken, verifyAdmin, async (req, res) => {
+      const productInfo = req.body;
+      const result = await productsCollection.insertOne(productInfo);
+      res.send(result);
+    })
+
+    // deleting product form the products server (ADMIN ONLY)
+    app.delete('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Updating a product info (ADMIN ONLY)
+    // Getting the info first
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+    // Now patching the info
+    app.patch('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const product = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          name: product.name,
+          category: product.category,
+          price: product.price,
+          fit: product.fit,
+          sizes: product.sizes,
+          colors: product.colors,
+          stock: product.stock,
+          image1: product.image1,
+          image2: product.image2,
+          image3: product.image3
+        }
+      }
+      const result = await productsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // Getting products details for the Product Details page when clicked to View Details
+    app.get('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
+    // Adding products to Cart
+    app.post('/cart', async (req, res) => {
+      const productData = req.body;
+      const result = await cartCollection.insertOne(productData);
+      res.send(result);
+    })
+
+    // Getting the products added to the cart (Specific user's data)
+    app.get('/cart', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = cartCollection.find(query);
+      const allProductsInCart = await cursor.toArray();
+      res.send(allProductsInCart);
+    })
+
+    // Delete cart item from user dashboard cart
+    app.delete('/cart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // posting orders to orders server
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // getting the order info for the specific user
+    app.get('/orders', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = ordersCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+
+    // cancel order from user dashboard orders
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Get all orders (ADMIN ONLY)
+    app.get('/allOrders', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await ordersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Order cancel or confirm (ADMIN ONLY)
+    app.patch('/orders/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const { status } = req.body;
+      const result = await ordersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { status: status } }
+      );
+      res.send(result);
+    });
+
+    // order delete (ADMIN ONLY)
+    app.delete('/orders/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const result = await ordersCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
